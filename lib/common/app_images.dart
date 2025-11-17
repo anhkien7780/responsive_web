994c12345eb1a25e9f591090791355dbdb41cae9
kiennv3 <kiennv3@newwave.vn>
2025-11-17 09:24:43 +0700

    Initial commit

---
+++ lib/common/app_images.dart
@@ -0,0 +1,14 @@
+class AppImages{
+  AppImages._();
+  static final imgHeader = "assets/images/img_header.png";
+  static final imgIllustration1 = "assets/images/img_illustration_1.png";
+  static final imgIllustration2 = "assets/images/img_illustration_2.png";
+  static final logoAkansa = "assets/images/logo_akansa.png";
+  static final logoAktas = "assets/images/logo_aktas.png";
+  static final logoAmazon = "assets/images/logo_amazon.png";
+  static final logoBilgi = "assets/images/logo_bilgi.png";
+  static final logoBookMyShow = "assets/images/logo_book_my_show.png";
+  static final logoBorusan = "assets/images/logo_borusan.png";
+  static final logoOla = "assets/images/logo_ola.png";
+  static final logoTumunuGor = "assets/images/logo_tumunu_gor.png";
+}
---
+++ lib/common/app_svgs.dart
@@ -0,0 +1,11 @@
+class AppSvgs{
+  AppSvgs._();
+
+  static final icChart = "ic_chart.svg";
+  static final icCircleChart = "ic_circle_chart.svg";
+  static final icCrown = "ic_crown.svg";
+  static final icFigma = "ic_figma.svg";
+  static final icFile = "ic_file.svg";
+  static final icGroup = "ic_group.svg";
+  static final icStar = "ic_star.svg";
+}
