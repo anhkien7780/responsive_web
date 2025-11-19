827ef17f459e7e44b63611e9ffb2e60ed5273499
kiennv3 <kiennv3@newwave.vn>
2025-11-19 16:14:05 +0700

    feat: add skill list

--- lib/ui/page/home/widgets/skill_list.dart
+++ lib/ui/page/home/widgets/skill_list.dart
@@ -60,28 +60,87 @@ class SkillList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: ResponsiveValue<double>(
+          context,
+          defaultValue: 120,
+          conditionalValues: [Condition.equals(name: MOBILE, value: 60)],
+        ).value,
+        horizontal: ResponsiveValue<double>(
+          context,
+          defaultValue: 20,
+          conditionalValues: [
+            Condition.largerThan(name: DESKTOP, value: 143),
+            Condition.largerThan(name: TABLET, value: 130),
+          ],
+        ).value,
+      ),
       constraints: const BoxConstraints(maxWidth: 1153),
-      child: ResponsiveGridView.builder(
-        alignment: Alignment.center,
-        itemCount: skills.length,
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate: ResponsiveGridDelegate(
-          crossAxisExtent: 300,
-          mainAxisSpacing: 88,
-          crossAxisSpacing: 100,
-          childAspectRatio: 1.37,
-        ),
-        itemBuilder: (context, index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(skills[index].iconPath),
-              Text(skills[index].name, textAlign: TextAlign.center),
-              Text(skills[index].description, textAlign: TextAlign.center),
-            ],
-          );
-        },
+      child: Column(
+        children: [
+          Text(
+            "Kalite ve Süreç Yönetimi",
+            style: TextStyle(
+              fontSize: ResponsiveValue<double>(
+                context,
+                defaultValue: 36,
+                conditionalValues: [Condition.equals(name: MOBILE, value: 24)],
+              ).value,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: ResponsiveValue<double>(
+              context,
+              defaultValue: 30,
+              conditionalValues: [
+                Condition.smallerThan(name: TABLET, value: 15),
+              ],
+            ).value,
+          ),
+          Text(
+            "Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.",
+            style: TextStyle(
+              fontSize: ResponsiveValue<double>(
+                context,
+                defaultValue: 24,
+                conditionalValues: [Condition.equals(name: MOBILE, value: 12)],
+              ).value,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: ResponsiveValue<double>(
+              context,
+              defaultValue: 90,
+              conditionalValues: [
+                Condition.smallerThan(name: TABLET, value: 40),
+              ],
+            ).value,
+          ),
+          ResponsiveGridView.builder(
+            alignment: Alignment.center,
+            itemCount: skills.length,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: ResponsiveGridDelegate(
+              crossAxisExtent: 300,
+              mainAxisSpacing: 88,
+              crossAxisSpacing: 100,
+              childAspectRatio: 1.37,
+            ),
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(skills[index].iconPath),
+                  Text(skills[index].name, textAlign: TextAlign.center),
+                  Text(skills[index].description, textAlign: TextAlign.center),
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
